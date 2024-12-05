221107b870753403f863288900f93b3891131d47
Yuuki Udagawa <s23C1022VL@s.chibakoudai.jp>
2024-12-05 12:36:45 +0900

    W

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
+class Listener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.pub = self.create_subscription(Int16, "countup", self.cb, 10)
 
-rclpy.init()
-node = Node("listener")
-
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg.age)
-
+    def cb(self, msg):
+        self.get_logger().info("Listen: %s" % msg.data)
 
 def main():
-    pub = node.create_subscription(Person, "person", cb, 10)
+    rclpy.init()
+    node = Listener()
     rclpy.spin(node)
